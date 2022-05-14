a8081bc86c847335511e36404bca87605c131535
ronchitech <ronchibusiness@gmail.com>
2022-05-14 22:16:23 +0800

    convert the CardList Component into Functional, used destructuring and implicit return

--- src/components/CardList/card-list.component.jsx
+++ src/components/CardList/card-list.component.jsx
@@ -1,11 +1,6 @@
-import { Component } from 'react';
-
-class CardList extends Component {
-  render() {
-    return this.props.monsters.map((monster) => {
-      return <h2 key={monster.id}>{monster.name}</h2>;
-    });
-  }
-}
+const CardList = ({ monsters }, forwardRef) =>
+  monsters.map((monster) => {
+    return <h2 key={monster.id}>{monster.name}</h2>;
+  });
 
 export default CardList;
